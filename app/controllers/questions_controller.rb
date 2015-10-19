0c1b0841f568924265e58d58cc578fb257b90e84
Bunker & Ryan Heupler <student@epicodus.com>
2015-10-19 09:45:49 -0700

    add delete button and route for questions model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,10 +33,13 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to questions_path
+  end
 
-
-
-
+  
 private
   def question_params
     params.require(:question).permit(:title, :content)

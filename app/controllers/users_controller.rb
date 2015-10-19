7ee2a1fc57ed3512493ac126757b457495de7887
Bunker & Ryan Heupler <student@epicodus.com>
2015-10-19 16:22:56 -0700

    styling for index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
   end
 
@@ -30,6 +34,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @users = User.all
   end
 
   private

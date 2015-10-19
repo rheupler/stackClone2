73f9b1ee406b0df0c7cbd0e1a5e98d0fa40d6ccc
Bunker & Ryan Heupler <student@epicodus.com>
2015-10-19 09:42:28 -0700

    add edit route and view page for questions model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,6 +20,19 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
 
 
 

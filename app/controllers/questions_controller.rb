d7e88029a475a499d8b1314c7abe88361dd2210a
Bunker & Ryan Heupler <student@epicodus.com>
2015-10-19 09:37:56 -0700

    add show page for individual questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,33 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+
+
+
+
+private
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
+
+
+end
